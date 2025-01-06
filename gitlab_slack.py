ff34ec0a1fc19be05fd01deedde8f8ba2cf0352b
TW - IT - BE - Willy Cheng <willy.cheng@shoalter.com>
2025-01-06 15:29:13 +0800

    fix gitlab notification content

--- gitlab_slack.py
+++ gitlab_slack.py
@@ -8,8 +8,8 @@ from service import slack_svc
 def pushed_commit(event, context):
     request_body = event.get('body', '')
     body = json.loads(request_body)
-    print(request_body)
-    commit_message = f"Following commit has been pushed to {request_body["ref"].replace("refs/heads/", "")}\r\n```"
+    print(body)
+    commit_message = f"Following commit has been pushed to {body["ref"].replace("refs/heads/", "")}\r\n```"
     for commit in body["commits"]:
         commit_message = f"{commit_message}{commit["title"]}\r\n"
     commit_message = f"{commit_message}```"
